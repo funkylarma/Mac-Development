4d38bd39fd9480cd5d7050e583eafc153ace8f99
Adam Chamberlin <adam@shibbyonline.co.uk>
2011-10-11 20:44:13 +0100

    simple hello world command line tutorial

---
+++ Learn C on the Mac/Chapter2/hello/hello/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Adam Chamberlin on 11/10/2011.
+//  Copyright 2011 Stour Valley Designs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
