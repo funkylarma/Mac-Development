320b6703de21d0ee66c03d541d43c998b4b6b947
Adam Chamberlin <adam@shibbyonline.co.uk>
2011-10-13 22:30:16 +0100

    example of backslashes in action

---
+++ Learn C on the Mac/Chapter5/slasher/slasher/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  slasher
+//
+//  Created by Adam Chamberlin on 13/10/2011.
+//  Copyright 2011 Stour Valley Designs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    printf("0000000000\r");
+    printf("11111\n");
+    printf("0000\b\b11\n");
+    printf("Here's a blackslash...\\...for you.\n");
+    printf("Here's a double quote...\"...for you.\n");
+    printf("Here's a few tabs...\t\t\t\t...for you.\n");
+    return 0;
+}
+
