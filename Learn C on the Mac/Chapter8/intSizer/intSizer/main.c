73fbb23b40f2948569f744205c56fe5a5da31e87
Adam Chamberlin <adam@shibbyonline.co.uk>
2011-10-29 13:27:01 +0100

    memory allocation and size of ints

---
+++ Learn C on the Mac/Chapter8/intSizer/intSizer/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  intSizer
+//
+//  Created by Adam Chamberlin on 29/10/2011.
+//  Copyright (c) 2011 Stour Valley Designs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    printf( "sizeof( char ) = %d\n", (int)sizeof( char ) );
+	printf( "sizeof( short ) = %d\n", (int)sizeof( short ) );
+	printf( "sizeof( int ) = %d\n", (int)sizeof( int ) );
+	printf( "sizeof( long ) = %d\n", (int)sizeof( long ) );
+	
+	return 0;    return 0;
+}
+
