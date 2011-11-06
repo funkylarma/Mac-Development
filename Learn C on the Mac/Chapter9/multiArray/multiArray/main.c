2bd6a792bdb8c7e40c9c16cebcf3f8a20cd85afe
Adam Chamberlin <adam@shibbyonline.co.uk>
2011-11-06 16:51:04 +0000

    Added error checking

--- Learn C on the Mac/Chapter9/multiArray/multiArray/main.c
+++ Learn C on the Mac/Chapter9/multiArray/multiArray/main.c
@@ -7,34 +7,75 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
 #define kMaxDVDs 4
 #define kMaxTitleLength 256
+#define kError_printf -1
+#define kError_fgets -2
+#define kError_none 0
 
-void PrintDVDTitle(int dvdNum, char title[][kMaxTitleLength]);
+int PrintDVDTitle(int dvdNum, char title[][kMaxTitleLength]);
+void ReplaceReturnAtEndOfString(char *theString);
 
 int main (int argc, const char * argv[])
 {
     
     char title[kMaxDVDs][kMaxTitleLength];
     short dvdNum;
+    char *result;
+    int error;
     
-    printf("The title array takes up %ld bytes of memory.\n\n", sizeof(title));
+    error = printf("The title array takes up %ld bytes of memoery.\n\n", sizeof(title));
     
+    if (error < 0) {
+        return kError_printf;
+    }
+        
     for (dvdNum = 0; dvdNum < kMaxDVDs; dvdNum++) {
-        printf("Title of DVD #%d: ", dvdNum + 1);
-        fgets(title[dvdNum], kMaxTitleLength, stdin);
+        error = printf("Title of DVD #%d: ", dvdNum + 1);
+    
+        if (error < 0) {
+            return kError_printf;
+        }
+        
+        result = fgets(title[dvdNum], kMaxTitleLength, stdin);
+        
+        if (result == NULL) {
+            error = printf("*** Error reported by fgets ***");
+            return kError_fgets;
+        }
+        
+        ReplaceReturnAtEndOfString(title[dvdNum]);
     }
     
-    printf("----\n");
+    error = printf("----\n");
+    
+    if (error < 0) {
+        return kError_printf;
+    }
     
     for (dvdNum = 0; dvdNum < kMaxDVDs; dvdNum++) {
-        PrintDVDTitle(dvdNum, title);
+        error = PrintDVDTitle(dvdNum, title);
+        if (error != kError_none) {
+            return error;
+        }
     }
     
     return 0;
 }
 
-void PrintDVDTitle(int dvdNum, char title[][kMaxTitleLength]) {
-    printf("Title of DVD #%d: %s\n", dvdNum + 1, title[dvdNum]);
+int PrintDVDTitle(int dvdNum, char title[][kMaxTitleLength]) {
+    int error;
+    error = printf("Title of DVD #%d: %s\n", dvdNum + 1, title[dvdNum]);
+    if (error < 0) {
+        return  kError_printf;
+    } else {
+        return kError_none;
+    }
+}
+
+void ReplaceReturnAtEndOfString(char *theString) {
+    int length = (int)strlen(theString);
+    theString[length - 1] = '\0';
 }
