09631efe101fbf9497df1cd44cccac0a36cebd4a
Adam Chamberlin <adam@shibbyonline.co.uk>
2011-11-13 14:00:02 +0000

    Taste of Objective-C

---
+++ Learn C on the Mac/Chapter12/tasteOfObjc/tasteOfObjc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  tasteOfObjc
+//
+//  Created by Adam Chamberlin on 13/11/2011.
+//  Copyright (c) 2011 Stour Valley Designs. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
