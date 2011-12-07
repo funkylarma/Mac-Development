a24bb6f73b4de4a58af0bcd5499789c6dec3ae1f
Adam Chamberlin <adam@shibbyonline.co.uk>
2011-12-07 20:29:12 +0000

    Challenge - Sine of 1 Radian

---
+++ Objective-C Programming/Chapter6/Number/Number/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Number
+//
+//  Created by Adam Chamberlin on 07/12/2011.
+//  Copyright (c) 2011 Stour Valley Designs. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main (int argc, const char * argv[])
+{
+    
+    printf("The sine of 1 radian is: %.3f\n", sin(1));
+    
+    return 0;
+}
+
