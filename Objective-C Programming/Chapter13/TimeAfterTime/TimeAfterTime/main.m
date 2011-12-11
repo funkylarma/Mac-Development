19c5572877602364f8be64d3286fa2f75d2f8cf8
Adam Chamberlin <adam@shibbyonline.co.uk>
2011-12-11 15:57:29 +0000

    Multiple arguments in messages

---
+++ Objective-C Programming/Chapter13/TimeAfterTime/TimeAfterTime/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Adam Chamberlin on 11/12/2011.
+//  Copyright (c) 2011 Stour Valley Designs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSDate *now = [[NSDate alloc] init];
+        NSLog(@"The date is %@", now);
+        
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since the start of 1970.", seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds it will be %@", later);
+        
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSUInteger day = [cal ordinalityOfUnit:NSDayCalendarUnit 
+                                        inUnit:NSMonthCalendarUnit 
+                                       forDate:now];
+        NSLog(@"This is day %lu of the month", day);
+        
+    }
+    return 0;
+}
+
