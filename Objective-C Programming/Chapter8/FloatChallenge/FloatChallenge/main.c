6ce92cecc43aa99180b0814ec1e4eaf6a7fa2e68
Adam Chamberlin <adam@shibbyonline.co.uk>
2011-12-11 13:38:44 +0000

    Challenge - Size of a float

---
+++ Objective-C Programming/Chapter8/FloatChallenge/FloatChallenge/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  FloatChallenge
+//
+//  Created by Adam Chamberlin on 11/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("The size of a float is %zu bytes\n", sizeof(float));
+    printf("A pointer to a float is %zu bytes\n", sizeof(float *));
+    return 0;
+}
+
