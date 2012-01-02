0c9cd22d833a2d6683b7178baadf272157928061
Adam Chamberlin <adam@shibbyonline.co.uk>
2012-01-02 17:10:39 +0000

    Reading files to NSString

--- Objective-C Programming/Chapter23/Stringz/Stringz/main.m
+++ Objective-C Programming/Chapter23/Stringz/Stringz/main.m
@@ -13,26 +13,14 @@ int main (int argc, const char * argv[])
 
     @autoreleasepool {
         
-        NSMutableString *str = [[NSMutableString alloc] init];
-        for (int i = 0; i < 10; i++) {
-            [str appendString:@"Adam is cool!\n"];
-        }
-        
-        //Declare a pointer to an NSError object, but don't instantiate it.
-        //The NSError instance will only be created if there is, in fact, an error.
         NSError *error = nil;
-        
-        //Pass the NSError pointer by reference to the NSString method
-        BOOL success = [str writeToFile:@"/tmp/cool.txt"
-                             atomically:YES
-                               encoding:NSUTF8StringEncoding
-                                  error:&error];
-        
-        //Test the returned BOOL, and query the NSError if the write failed
-        if (success) {
-            NSLog(@"done writing /tmp/cool.txt");
+        NSString *str = [[NSString alloc] initWithContentsOfFile:@"/etc/resolv.conf"
+                                                        encoding:NSASCIIStringEncoding
+                                                           error:&error];
+        if (!str) {
+            NSLog(@"read failed: %@", [error localizedDescription]);
         } else {
-            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+            NSLog(@"resolv.conf looks like this: %@", str);
         }
         
     }
