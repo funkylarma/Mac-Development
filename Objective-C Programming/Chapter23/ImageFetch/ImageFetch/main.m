3ed46ee223d6d4b808a9c0c580ce2a335e8e67b1
Adam Chamberlin <adam@shibbyonline.co.uk>
2012-01-03 21:38:17 +0000

    Reading and writing NSData

---
+++ Objective-C Programming/Chapter23/ImageFetch/ImageFetch/main.m
@@ -0,0 +1,47 @@
+//
+//  main.m
+//  ImageFetch
+//
+//  Created by Adam Chamberlin on 03/01/2012.
+//  Copyright (c) 2012 Stour Valley Designs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSURL *url = [NSURL URLWithString:@"http://www.google.com/images/logos/ps_logo2.png"];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        NSError *error = nil;
+        NSData *data = [NSURLConnection sendSynchronousRequest:request
+                                             returningResponse:NULL
+                                                         error:&error];
+        
+        if (!data) {
+            NSLog(@"fetch failed: %@", error);
+            return 1;
+        }
+        
+        NSLog(@"The file is %lu bytes", [data length]);
+        
+        BOOL written = [data writeToFile:@"/tmp/google.png"
+                                 options:NSDataWritingAtomic
+                                   error:&error];
+        
+        if (!written) {
+            NSLog(@"write failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        
+        NSLog(@"Success!");
+        
+        NSData *readData = [NSData dataWithContentsOfFile:@"/tmp/google.png"];
+        NSLog(@"The file read from the disk has %lu bytes", [readData length]);
+        
+    }
+    return 0;
+}
+
