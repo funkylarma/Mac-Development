493693789579c36f84c4c39ff36ca27afad6d6ce
Adam Chamberlin <adam@shibbyonline.co.uk>
2012-01-22 15:53:54 +0000

    The Random number generator

---
+++ Cocoa Programming for Mac OSX/Chapter2/Random/Random/RandomAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RandomAppDelegate.h
+//  Random
+//
+//  Created by Adam Chamberlin on 22/01/2012.
+//  Copyright (c) 2012 Stour Valley Designs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RandomAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
