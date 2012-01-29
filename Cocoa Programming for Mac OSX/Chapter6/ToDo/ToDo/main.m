1150b48cd46f83c2dca91b06d35c8acf23956ca3
Adam Chamberlin <adam@shibbyonline.co.uk>
2012-01-29 21:04:12 +0000

    Challenge - Datasources

---
+++ Cocoa Programming for Mac OSX/Chapter6/ToDo/ToDo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ToDo
+//
+//  Created by Adam Chamberlin on 28/01/2012.
+//  Copyright (c) 2012 Stour Valley Designs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
