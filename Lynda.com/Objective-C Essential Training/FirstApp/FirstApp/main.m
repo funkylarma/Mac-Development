e96fea55daaeddd196ba7ee5e6333b552791723d
Adam Chamberlin <adam@shibbyonline.co.uk>
2012-08-02 12:57:40 +0100

    Lynda Objective-C training

---
+++ Lynda.com/Objective-C Essential Training/FirstApp/FirstApp/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Adam Chamberlin on 02/08/2012.
+//  Copyright (c) 2012 Stour Valley Designs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
---
+++ Lynda.com/Objective-C Essential Training/SecondApp/SecondApp/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  SecondApp
+//
+//  Created by Adam Chamberlin on 02/08/2012.
+//  Copyright (c) 2012 Stour Valley Designs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        int category = 47;
+        
+        switch (category) {
+            case 41:
+                NSLog(@"Category is 41");
+                break;
+                
+            default:
+                break;
+        }
+        
+    }
+    return 0;
+}
+
