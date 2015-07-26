720c6374a26b49b3632d24f873190adf33ce837f
Adam Chamberlin <adam@shibbyonline.co.uk>
2015-07-26 20:57:14 +0100

    Chapter 2

--- Cocoa Programming for Mac OSX/MyPlayground.playground/Contents.swift
+++ Cocoa Programming for Mac OSX/MyPlayground.playground/Contents.swift
@@ -16,7 +16,7 @@ var winningLotteryNumbers: Set<Int>
 let number = 42
 let fmStation = 91.1
 
-let countingUp = ["one", "two"]
+var countingUp = ["one", "two"]
 let nameByParkingSpace = [13: "Alice", 27: "Bob"]
 let secondElement = countingUp[1]
 
@@ -36,4 +36,62 @@ let floatFromLiteral = Float(3.14)
 let easyPi = 3.14
 let floatFromDouble = Float(easyPi)
 let floatingPi: Float = 3.14
+
+emptyString.isEmpty
+countingUp.count
+countingUp.append("three")
+let countingDown = countingUp.reverse()
+
+var anOptionalFloat: Float?
+var anOptionalArrayOfStrings: [String]?
+var anOptionalArrayOfOptionalStrings: [String?]?
+
+var reading1: Float?
+var reading2: Float?
+var reading3: Float?
+
+reading1 = 9.8
+reading2 = 9.2
+//reading3 = 9.7
+
+if let r1 = reading1, let r2 = reading2, let r3 = reading3 {
+  let avgReading = (r1 + r2 + r3) / 3
+} else {
+  let errorString = "Instrument reported a reading that was nil"
+}
+
+if let spaceAssignee = nameByParkingSpace[13] {
+  println("Key 13 was in the dictionary")
+}
+
+for (space, name) in nameByParkingSpace {
+  let permit = "Space \(space): \(name)"
+}
+
+enum PieType: Int {
+  case Apple = 0
+  case Cherry
+  case Pecan
+}
+
+let favoritePie = PieType.Apple
+
+var piesToBake: [PieType] = []
+piesToBake.append(.Cherry)
+
+let name: String
+switch favoritePie {
+  case .Apple:
+    name = "Apple"
+  case .Cheery:
+    name = "Cherry"
+  case .Pecan
+    name = "Pecan"
+}
+
+let pieRawValue = PieType.Pecan.rawValue
+if let pieType = PieType(rawValue: pieRawValue) {
+  // Got a valid 'pieType'
+}
+
 
