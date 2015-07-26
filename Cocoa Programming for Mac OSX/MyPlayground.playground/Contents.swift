27ee92812a09b569dbf95884d87cf462b2dbebfa
Adam Chamberlin <adam@shibbyonline.co.uk>
2015-07-26 07:11:31 +0100

    Introduction to Swift basics

---
+++ Cocoa Programming for Mac OSX/MyPlayground.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+str = "Hello, Swift"
+let constStr = str
+
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+var arrayOfInts: [Int]
+var dictionaryOfCapitalsByCountry: [String:String]
+var winningLotteryNumbers: Set<Int>
+
+let number = 42
+let fmStation = 91.1
+
+let countingUp = ["one", "two"]
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+let secondElement = countingUp[1]
+
+let emptyString = String()
+let emptyArrayOfInts = [Int]()
+let emptySetOfFloats = Set<Float>()
+
+let defaultNumber = Int()
+let defaultBool = Bool()
+
+let meaningOfLife = String(number)
+let availableRooms = Set([205, 411, 412])
+
+let defaultFloat = Float()
+let floatFromLiteral = Float(3.14)
+
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+let floatingPi: Float = 3.14
+
