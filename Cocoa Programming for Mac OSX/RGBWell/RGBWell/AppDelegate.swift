361dff9549331a89feaa6ff9199be07762820f9d
Adam Chamberlin <adam@shibbyonline.co.uk>
2015-11-25 22:14:07 +0000

    started with RGBWell

---
+++ Cocoa Programming for Mac OSX/RGBWell/RGBWell/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  RGBWell
+//
+//  Created by Adam Chamberlin on 25/11/2015.
+//  Copyright © 2015 Adam Chamberlin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  var mainWindowController: MainWindowController?
+  
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+    
+    // Create a window controller
+    let mainWindowController = MainWindowController()
+    
+    // Put the window controller on screen
+    mainWindowController.showWindow(self)
+    
+    // Set the property to point to the window controller
+    self.mainWindowController = mainWindowController
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Cocoa Programming for Mac OSX/RGBWell/RGBWell/MainWindowController.swift
@@ -0,0 +1,16 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by Adam Chamberlin on 25/11/2015.
+//  Copyright © 2015 Adam Chamberlin. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+  
+  override var windowNibName: String? {
+    return "MainWindowController"
+  }
+}
