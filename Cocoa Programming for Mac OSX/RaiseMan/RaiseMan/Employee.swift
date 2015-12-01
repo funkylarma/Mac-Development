9f0fc17596a8896ff52922f720080cb1d03f1288
Adam Chamberlin <adam@shibbyonline.co.uk>
2015-12-01 15:50:32 +0000

    building RaiseMan

---
+++ Cocoa Programming for Mac OSX/RaiseMan/RaiseMan/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by Adam Chamberlin on 01/12/2015.
+//  Copyright © 2015 Adam Chamberlin. All rights reserved.
+//
+
+import Foundation
+
+class Employee: NSObject {
+  var name: String? = "New Employee"
+  var raise: Float = 0.05
+}
