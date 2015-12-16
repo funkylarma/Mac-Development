5fdbf2cf0a3bb0a0e5721dd79a9cd40332f6f1e7
Adam Chamberlin <adam@shibbyonline.co.uk>
2015-12-16 13:11:01 +0000

    bronze challenge

--- Swift Programming/Variables.playground/Contents.swift
+++ Swift Programming/Variables.playground/Contents.swift
@@ -5,6 +5,8 @@ import Cocoa
 let numberOfStopLights: Int = 4
 var population: Int
 population = 5422
+var unEmployed: Int
+unEmployed = 12
 let townName: String = "Knowhere"
-let townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights."
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights. There are \(unEmployed) people unemployed."
 print(townDescription)
