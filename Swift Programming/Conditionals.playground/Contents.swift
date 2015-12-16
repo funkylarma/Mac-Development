c00f021d6726fd8c6e7496a41bc89b231f275d0c
Adam Chamberlin <adam@shibbyonline.co.uk>
2015-12-16 13:51:43 +0000

    bronze challenge

--- Swift Programming/Conditionals.playground/Contents.swift
+++ Swift Programming/Conditionals.playground/Contents.swift
@@ -8,6 +8,8 @@ var hasPostOffice: Bool = true
 
 if population < 1000 {
   message = "\(population) is a small town!"
+} else if population >= 50000 && population < 100000 {
+  message = "\(population) is a large town!"
 } else if population >= 10000 && population < 50000 {
   message = "\(population) is a medium town!"
 } else {
