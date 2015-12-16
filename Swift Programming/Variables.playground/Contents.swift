e47d47aabeb95b628ef722dd603c56ab1196bda3
Adam Chamberlin <adam@shibbyonline.co.uk>
2015-12-16 13:09:45 +0000

    contstants and variables

---
+++ Swift Programming/Variables.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStopLights: Int = 4
+var population: Int
+population = 5422
+let townName: String = "Knowhere"
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights."
+print(townDescription)
